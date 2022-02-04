d71b17221ac7d03174e96eb08dc1304a88564e61
Sang Dang <sangdth@gmail.com>
2022-02-04 10:25:36 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // In order to use mapbox styles v10, I have to
+  // turn off this swcMinify and use .babelrc
+  swcMinify: true,
   reactStrictMode: true,
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'mapbox-gl/dist/mapbox-gl.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
