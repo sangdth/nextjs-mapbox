606173b948368617e4a18a478f2c00ee532576b7
Sang Dang <sangdth@gmail.com>
2022-02-09 10:08:33 +0200

    it works on canary

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
-import ReactMapGL from 'react-map-gl';
+import { Map } from 'react-map-gl';
 
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const [viewport] = useState({
+  const [viewport, setViewport] = useState({
     width: 800,
     height: 600,
     latitude: 37.7577,
@@ -15,11 +15,14 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <ReactMapGL
+      <Map
         {...viewport}
         // mapStyle="mapbox://styles/mapbox/dark-v9" // WORKS
-        mapStyle="mapbox://styles/mapbox/dark-v10" // DOES NOT WORKS if swcMinify is `true`
-        mapboxApiAccessToken="pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg"
+        // DOES NOT WORKS if swcMinify is `true` on major version
+        // WORKS on 12.0.11-canary.9
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapboxAccessToken="pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg"
+        onMove={(event: any) => setViewport(event.viewState)}
       />
     </div>
   )
